b44d80c6854771b9fed87a438acf126f669e1890
maryamxim <maryamxiim@gmail.com>
2021-12-08 11:25:03 +0330

    💄Update style

--- src/App.js
+++ src/App.js
@@ -1,91 +1,164 @@
-import React, {useState} from "react";
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
 import Time from "./component/Time";
-
-
+import swal from "sweetalert";
 
 function App() {
+  const [valueStart, setvalueStart] = useState([
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+  ]);
+  const [valueEnd, setvalueEnd] = useState([
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+    "00:00",
+  ]);
+  const day = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
 
-const [valueStart , setvalueStart] = useState(["00:00", "00:00", "00:00","00:00", "00:00","00:00","00:00" ])
-const [valueEnd , setvalueEnd] = useState(["00:00" , "00:00", "00:00","00:00", "00:00","00:00","00:00"])
-const day = ["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"]
+  function UpdateChangeStart(value, xid) {
+    const x = [...valueStart];
+    x[xid] = value;
+    setvalueStart(x);
+  }
 
-function UpdateChangeStart(value , xid) {
-  const x = [...valueStart];
-  x[xid] = value;
-  setvalueStart(x)
-}
+  function UpdateChangeEnd(values, xids) {
+    const y = [...valueEnd];
+    y[xids] = values;
+    setvalueEnd(y);
+  }
 
-function UpdateChangeEnd(value , xid) {
-  const y = [...valueStart];
-  y[xid] = value;
-  setvalueEnd(y)
-}
+  function ShowList() {
+    swal("Please inter your name:", {
+      content: "input",
+    }).then((value) => {
+      swal(
+        "Hi" +
+          " " +
+          `${value} ` +
+          "\n" +
+          "These are your FreeTimes:" +
+          "\n" +
+          JSON.stringify(day[0]) +
+          ": " +
+          " From " +
+          "From" +
+          JSON.stringify(valueStart[0]) +
+          " To " +
+          JSON.stringify(valueEnd[0]) +
+          "\n" +
+          JSON.stringify(day[1]) +
+          ": " +
+          " From " +
+          JSON.stringify(valueStart[1]) +
+          " To " +
+          JSON.stringify(valueEnd[1]) +
+          "\n" +
+          JSON.stringify(day[2]) +
+          ": " +
+          " From " +
+          JSON.stringify(valueStart[2]) +
+          " To " +
+          JSON.stringify(valueEnd[2]) +
+          "\n" +
+          JSON.stringify(day[3]) +
+          ": " +
+          " From " +
+          JSON.stringify(valueStart[3]) +
+          " To " +
+          JSON.stringify(valueEnd[3]) +
+          "\n" +
+          JSON.stringify(day[4]) +
+          ": " +
+          " From " +
+          JSON.stringify(valueStart[4]) +
+          " To " +
+          JSON.stringify(valueEnd[4]) +
+          "\n" +
+          JSON.stringify(day[5]) +
+          ": " +
+          " From " +
+          JSON.stringify(valueStart[5]) +
+          " To " +
+          JSON.stringify(valueEnd[5]) +
+          "\n" +
+          JSON.stringify(day[6]) +
+          ": " +
+          " From " +
+          JSON.stringify(valueStart[6]) +
+          " To " +
+          JSON.stringify(valueEnd[6]) +
+          "\n"
+      );
+    });
+  }
 
-function ShowList() {
-  let Input = prompt("Please enter your name:", "MaryamXim");
-  let Inputs = alert("Hi" +" "+ Input + "\n" +
-  "These are your FreeTimes:" +"\n"+
-  JSON.stringify(day[0])+": "+" From "+ "From"+JSON.stringify(valueStart[0]) +" To "+ JSON.stringify(valueStart[0])+ "\n" +
-  JSON.stringify(day[1])+": "+" From "+JSON.stringify(valueStart[1]) +" To "+ JSON.stringify(valueStart[1])+ "\n"+
-  JSON.stringify(day[2])+": "+" From "+JSON.stringify(valueStart[2]) +" To "+ JSON.stringify(valueStart[2])+ "\n"+
-  JSON.stringify(day[3])+": "+" From "+JSON.stringify(valueStart[3]) +" To "+ JSON.stringify(valueStart[3])+ "\n"+
-  JSON.stringify(day[4])+": "+" From "+JSON.stringify(valueStart[4]) +" To "+ JSON.stringify(valueStart[4])+ "\n"+
-  JSON.stringify(day[5])+": "+" From "+JSON.stringify(valueStart[5]) +" To "+ JSON.stringify(valueStart[5])+ "\n"+
-  JSON.stringify(day[6])+": "+" From "+JSON.stringify(valueStart[6]) +" To "+ JSON.stringify(valueStart[6])+ "\n" )
-}
-
-function Delete(){
-  setvalueStart(["00:00", "00:00", "00:00","00:00", "00:00","00:00","00:00","00:00"]);
-  setvalueEnd(["00:00", "00:00", "00:00","00:00", "00:00","00:00","00:00","00:00"]);
-      // window.localStorage.clear()
-      // window.location.reload()
-}
+  function Delete() {
+    setvalueStart([
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+    ]);
+    setvalueEnd([
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+      "00:00",
+    ]);
+  }
 
   return (
-    <div className="App">
-      <div className="App-header">
-     <Time day={day[0]}
-      valueStart={valueStart[0]}
-      valueEnd={valueEnd[0]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,0)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,0)}/>
-     <Time day={day[1]}
-      valueStart={valueStart[1]}
-      valueEnd={valueEnd[1]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,1)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,1)}/>
-     <Time day={day[2]}
-      valueStart={valueStart[2]}
-      valueEnd={valueEnd[2]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,2)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,2)}/>
-     <Time day={day[3]}
-      valueStart={valueStart[3]}
-      valueEnd={valueEnd[3]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,3)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,3)}/>
-     <Time day={day[4]}
-      valueStart={valueStart[4]}
-      valueEnd={valueEnd[4]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,4)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,4)}/>
-      <Time day={day[5]}
-      valueStart={valueStart[5]}
-      valueEnd={valueEnd[5]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,5)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,5)}/>
-      <Time day={day[6]}
-      valueStart={valueStart[6]}
-      valueEnd={valueEnd[6]}
-      onChangeValueStart={ev => UpdateChangeStart(ev.target.value,6)}
-      onChangeValueEnd= {ev => UpdateChangeEnd(ev.target.value,6)} />
+    <div>
+      <div className="Totalss">
+        <div className="Total">
+          <div class="Header">Set standard Hours</div>
+          <div class="Caption">Configure the standard hours of operation</div>
+        </div>
+        <div className="App">
+          <div className="App-header">
+            {day.map((time, index) => (
+              <Time
+                day={time}
+                valueStart={valueStart[index]}
+                valueEnd={valueEnd[index]}
+                onChangeValueStart={(ev) =>
+                  UpdateChangeStart(ev.target.value, index)
+                }
+                onChangeValueEnd={(e) => UpdateChangeEnd(e.target.value, index)}
+              />
+            ))}
 
- <div className="buttons">
-  <button onClick={Delete}>Cancel</button>
-<button onClick={ShowList}> Save scheduler </button>
-</div>
-
+            <div className="buttons">
+              <button onClick={Delete}>Cancel</button>
+              <button onClick={ShowList}> Save scheduler </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/component/Time.js
+++ src/component/Time.js
@@ -1,36 +1,37 @@
 import React from "react";
-import "./time.css"
+import "./time.css";
 
-function Time({day , valueStart, valueEnd,onChangeValueStart, onChangeValueEnd}) {
-   
-
-    return(
-
-        <div className="total">
-        <div>{day}</div>
-   <div><input
-        type="time"
-        onChange={onChangeValueStart}
-        min="00:00"
-        max="23:59"
-        step="60"
-        value={valueStart}
-      />
-      &nbsp;
-       To 
-       &nbsp;
-       <input
-        type="time"
-        onChange={onChangeValueEnd}
-        min="00:00"
-        max="23:59"
-        step="60"
-        value={valueEnd}
-      /> </div>
+function Time({
+  day,
+  valueStart,
+  valueEnd,
+  onChangeValueStart,
+  onChangeValueEnd,
+}) {
+  return (
+    <div className="total">
+      <div>{day}</div>
+      <div>
+        <input
+          type="time"
+          onChange={onChangeValueStart}
+          min="00:00"
+          max="23:59"
+          step="60"
+          value={valueStart}
+        />
+        &nbsp; To &nbsp;
+        <input
+          type="time"
+          onChange={onChangeValueEnd}
+          min="00:00"
+          max="23:59"
+          step="60"
+          value={valueEnd}
+        />{" "}
+      </div>
     </div>
-    )
+  );
 }
-
-
 
 export default Time;
